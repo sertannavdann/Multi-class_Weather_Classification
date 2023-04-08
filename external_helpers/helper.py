c7d9cb1bd74ada0156fe6badd85d3ee2a3120a8a
Sertan Avdan <sertannavdann@gmail.com>
2023-04-07 21:11:02 -0400

    VAE Generator

---
+++ external_helpers/helper.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+path = 'data'
+classes = ['cloudy', 'rain', 'shine', 'sunrise']
+
+def get_class_name(file_name, classes):
+    name_without_extension = file_name.split('.')[0]
+    for class_name in classes:
+        if class_name in name_without_extension.lower():
+            return class_name
+    return None
+
+
+def organize_files(path, classes):
+    files = os.listdir(path)
+    for file in files:
+        class_name = get_class_name(file, classes)
+        if class_name is not None:
+            class_folder = os.path.join(path, class_name)
+            if not os.path.exists(class_folder):
+                os.makedirs(class_folder)
+            # move the file to the corresponding class folder
+            source_path = os.path.join(path, file)
+            destination_path = os.path.join(class_folder, file)
+            shutil.move(source_path, destination_path)
+
+organize_files(path=path, classes=classes)
